80f69526135c713e32cb6a176f14b68a8f815af2
huahua <1521972865@qq.com>
2022-04-18 15:37:10 +0800

    first commit

---
+++ src/main/java/com/zixi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zixi.git;
+
+/**
+ * @author hua
+ * @create 2022-04-18-15:30
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
